1942ae7f4fd8dffd3da8159fc6eab69854131d76
Sebastian Doreste <87989566+Japexican@users.noreply.github.com>
2021-11-19 22:11:22 -0600

    text saves to local storage and persists after refresh

--- script.js
+++ script.js
@@ -2,20 +2,38 @@ const checkHour = () => {
     const currHour = moment().hours();
     document.querySelectorAll("[data-hour]").forEach((item, i) => {
         const hour = item.getAttribute("data-hour");
-        if (hour < currHour){
-        item.classList.add("past")
-    }else if(hour == currHour){
-        item.classList.remove("past")
-        item.classList.add("present")
-    }else {
-        item.classList.remove("past")
-        item.classList.remove("present")
-        item.classList.add("future")
-    }
-});
+        if (hour < currHour) {
+            item.classList.add("past");
+        } else if (hour == currHour) {
+            item.classList.remove("past");
+            item.classList.add("present");
+        } else {
+            item.classList.remove("past");
+            item.classList.remove("present");
+            item.classList.add("future");
+        }
+    });
 };
-setInterval(checkHour,60000)
-checkHour()
+setInterval(checkHour, 60000);
+checkHour();
 
-const currDay = new Date().toDateString().split(" ").splice(0,3).join(", ")
-document.querySelector("#currentDay").innerHTML=`Today is ${currDay}.`
+const currDay = new Date().toDateString().split(" ").splice(0, 3).join(", ");
+document.querySelector("#currentDay").innerHTML = `Today is ${currDay}.`;
+
+document.querySelectorAll(".saveBtn").forEach(function (element) {
+    element.addEventListener("click", function () {
+        const hour = this.parentElement.getAttribute("data-hour");
+        const value = this.previousElementSibling.value;
+        localStorage.setItem(hour,value);
+        document.querySelector(".notification").classList.add("show")
+        setTimeout(function(){
+            document.querySelector(".notification").classList.remove("show")
+        }, 2000)
+    });
+});
+
+document.querySelectorAll("[data-hour]").forEach(function(element) {
+    const hour =  element.getAttribute("data-hour")
+    const value = localStorage.getItem(hour)
+    element.children[1].value = value
+})
